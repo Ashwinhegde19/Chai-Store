2b039ca7d365b3810d1fd58cff55df7805b17ea3
ashwinhegde19 <ashwinhegde19@gmail.com>
2024-06-08 11:51:36 +0530

    add forms

--- chai/urls.py
+++ chai/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.allchai, name='allchai'),
     path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
+    path('chai_stores/', views.chai_store_view, name='chai_stores'),
     
 ]
